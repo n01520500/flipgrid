43c9c9618b31230e02b0b9de0ba7ce5f8b48a52e
n01520500 <n01520500@humbermail.ca>
2022-06-02 13:11:10 -0700

    msg

--- Flipgrid.py
+++ Flipgrid.py
@@ -1,10 +1,11 @@
 import requests
-city = 'london'
+city = 'Vancouver'
 api_url = 'https://api.api-ninjas.com/v1/airquality?city={}'.format(city)
 response = requests.get(api_url, headers={'X-Api-Key': 'WZHHxfGa8xvgj+1pS1LyRA==QSvRML5qX92q2EkM'})
 if response.status_code == requests.codes.ok:
     print(response.text)
 else:
     print("Error:", response.status_code, response.text)
+    print 
     
     
